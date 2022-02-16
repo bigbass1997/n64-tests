68e4a9feb01529f4752bf0c530646654c20cd086
bigbass1997 <bigbass1997.website@gmail.com>
2022-02-15 22:43:09 -0600

    Cleaned code and added formatting examples.

--- MinimalTemplate/src/main.asm
+++ MinimalTemplate/src/main.asm
@@ -6,44 +6,27 @@ fill 0x00101000 // Set ROM Size // 1024 KB + 4 KB = 1028 KB = 0x00101000 bytes
 origin 0x00000000
 base 0x80000000
 
+include "header.asm"
+insert "n64_bootcode.bin"
+
+
+//// Everything below this point is what's absolutely required for this code to work by itself. ////
+
+
 constant zero(0)
-constant at(1)
-constant v0(2)
-constant v1(3)
-constant a0(4)
-constant a1(5)
-constant a2(6)
 constant a3(7)
 constant t0(8)
 constant t1(9)
 constant t2(10)
 constant t3(11)
 constant t4(12)
-constant t5(13)
-constant t6(14)
 constant t7(15)
-constant s0(16)
-constant s1(17)
-constant s2(18)
-constant s3(19)
-constant s4(20)
-constant s5(21)
-constant s6(22)
-constant s7(23)
 constant t8(24)
 constant t9(25)
-constant k0(26)
-constant k1(27)
-constant gp(28)
-constant sp(29)
-constant s8(30)
-constant ra(31)
+constant fp(30) // pointer to framebuffer
 
-constant fp(s8) // frame pointer (being used as framebuffer pointer)
-constant BUF_BASE(0xA0140000)
-
-include "header.asm" // DEBUG ONLY
-insert "n64_bootcode.bin" // DEBUG ONLY
+constant BUF_BASE(0xA0140000)  // base address for the framebuffer
+constant CHAR_SPACING(1)  // number of pixels of whitespace between each printed character
 
 macro SWOffset(value, temp_reg, offset, base_reg) {
     la {temp_reg}, {value}
@@ -70,16 +53,16 @@ Start:
     
     
     // --- Clear Framebuffer --- // This section is optional, but recommended for a clean display
-    addu s4, zero, fp
-    la s3, (4 * 320 * 240)
-    addu s4, s4, s3
+    addu t1, zero, fp
+    la t0, (4 * 320 * 240)
+    addu t1, t1, t0
     
-    addu s3, zero, fp
-    la s2, 0x222222FF // Background color
+    addu t0, zero, fp
+    la t2, 0x222222FF // Background color
 Clear:
-    sw s2, 0(s3)
-    bne s4, s3, Clear
-    addi s3, s3, 4
+    sw t2, 0(t0)
+    bne t1, t0, Clear
+    addi t0, t0, 4
     
     
     // --- Print Message --- //
@@ -89,11 +72,10 @@ Clear:
     la t0, Text
     la t9, TextEnd
     
-StartPrint:
+PrintText:
     addiu t7, zero, 8
 PrintChar:
     addi t7, t7, -1
-    
     lbu t3, 0(t0) // Load row
     addiu t0, t0, 1
     addiu t8, zero, 8
@@ -114,19 +96,34 @@ PrintSkipPixel:
     bne t7, zero, PrintChar
     nop
     
-    addi t1, t1, -(4 * 320 * 8) + 36
-    bne t0, t9, StartPrint
+    addi t1, t1, -(4 * 320 * 8) + (4 * (8 + CHAR_SPACING))
+    bne t0, t9, PrintText
     nop
     
     
     
-    // --- Spin Forever --- //
+    // --- Loop Forever --- //
 Spin:    
     j Spin
     nop
     
     
+    
+// This text data is a sequence of characters (images), where each bit represents a pixel that is either on (1) or off (0).
+// Each "character" is 8 x 8 bits. Each set of 8 bits is a row of pixels, and each set of 8 rows is a character.
+
+// Example, the letter 'A' could be manually created like so:
+// db 0b00011000
+// db 0b00100100
+// db 0b01000010
+// db 0b10000001
+// db 0b11111111
+// db 0b10000001
+// db 0b10000001
+// db 0b10000001
+
 Text:
-    insert TestText, "text.bin"
+    insert "text.bin" // binary format
+    // include "text.inc" // assembly format
 TextEnd:
     nop
